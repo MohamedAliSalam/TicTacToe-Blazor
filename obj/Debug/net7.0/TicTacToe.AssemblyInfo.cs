207e86d689e2bfe1241ac66054f4f404adbe9938
MohamedAliSalam <mhdalisalam@gmail.com>
2024-03-07 19:37:28 +0200

    2 player mode

--- obj/Debug/net7.0/TicTacToe.AssemblyInfo.cs
+++ obj/Debug/net7.0/TicTacToe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b594157fe1e6c68725ad73ac17fdd9665665dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
